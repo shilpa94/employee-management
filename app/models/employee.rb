c814edc113301f36372cd00cf73fb630a2f19170
Shilpa k <shilpasiva1510@gmail.com>
2019-02-03 20:57:54 +0530

    validation and ui change

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ApplicationRecord
 	validates :firstname, :lastname, :gender, :address, :department, presence:true
 	validates :email, :phone, :employee_id, presence:true, uniqueness:true
+	validates :phone, length: { is: 10 }
 end
